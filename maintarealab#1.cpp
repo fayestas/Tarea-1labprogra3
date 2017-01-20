e52844695d5f943649c5d97c1faae59b80851026
fayestas <ayestasfrances@unitec.edu>
2017-01-20 14:41:06 -0600

    Frances
    tarea#1 lab progra3

---
+++ maintarealab#1.cpp
@@ -0,0 +1,128 @@
+#include "Evaluador.h"
+#include <iostream>
+using namespace std;
+
+//Devuelve la suma de a (dado) y b (dado)
+int sumar(int a, int b)
+{
+    return a+b;
+}
+
+//Devuelve la resta de a (dado) y b (dado)
+int restar(int a, int b)
+{
+    return a-b;
+}
+
+//Devuelve la multiplicacion de a (dado) y b (dado)
+int multiplicar(int a, int b)
+{
+    return a*b;
+}
+
+//Devuelve la division de a (dado) y b (dado)
+int dividir(int a, int b)
+{
+    return a/b;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMayor(int a, int b)
+{
+    if (a<b)
+       return b;
+    else
+        return a;
+}
+
+//Devuelve el numero mayor entre a (dado) y b (dado)
+int getMenor(int a, int b)
+{
+    if (a>b){
+       return b;
+    }else{
+        return a;
+    }
+}
+
+//Devuelve el numero mayor entre a (dado), b (dado) y c (dado)
+int getMayor(int a, int b, int c)
+{
+    if(a>b && a>c){
+        cout<<"el numero mayor es:"<<a<<endl;
+    }else if (b>a && b>c){
+    cout<<"el numero mayor es:"<<b<<endl;
+    }else if(c>a&&c>b){
+    cout<<"el numero mayor es:"<<c<<endl;
+    }
+    return 0;
+}
+
+//Establece el valor (dado) en el arreglo (dado) en el indice posicion (dado)
+void setValor(int arreglo[], int valor, int posicion)
+{
+    arreglo[posicion]=valor;
+}
+
+//Obtiene y devuelve el valor del arreglo (dado) en el indice posicion (dado)
+int getValor(int arreglo[], int posicion)
+{
+    return arreglo[posicion];
+}
+
+//Devuelve el numero mayor del arreglo (dado) que contiene tamano (dado) elementos
+int getMayor(int arreglo[], int tamano)
+{
+   int mayor =0;
+    int contador;
+     for (contador=0; contador<tamano; contador++) {
+    if (arreglo[contador]>mayor)
+    {
+    mayor=arreglo[contador];
+    }
+    }
+
+    return mayor;
+}
+
+//Devuelve el numero menor del arreglo (dado) que contiene tamano (dado) elementos
+int getMenor(int arreglo[], int tamano)
+{
+
+    int menor=99;
+    int contador;
+    for (contador=0; contador<tamano; contador++)
+    {
+    if (arreglo[contador]<menor)
+    {
+    menor = arreglo[contador];
+    }
+    }
+    return menor;
+}
+
+//Devuelve el promedio de los numeros del arreglo (dado) que contiene tamano (dado) elementos
+int getPromedio(int arreglo[], int tamano)
+{
+    int suma=0;
+   int promedio;
+   int contador;
+
+
+    for (contador=0;contador<tamano;contador++)
+   {
+
+   suma+=arreglo[contador];
+
+   promedio= suma/tamano;
+   }
+
+return promedio;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
